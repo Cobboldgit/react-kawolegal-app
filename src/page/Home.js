d5d02ef2e675e2771f5d1f0983691bbfa9667349
Cobboldgit <86422221+Cobboldgit@users.noreply.github.com>
2021-09-24 20:31:42 +0000

    create all the pages

---
+++ src/page/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <div className="hero-image">
+        <nav className="navbar navbar-light  navedit">
+          <Link className="navbar-brand" to="/">
+            <img
+              src="image/KAWO.png"
+              width="200px"
+              height="100px"
+              className="d-inline-block align-top"
+              alt="logo"
+            />
+          </Link>
+
+          <ul>
+            <li className="active-link">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/startups">All Startups</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="content">
+          <h1 className="text-center">KAWOLEGAL</h1>
+          <p className="text-center">
+            A collaborative ecosystem for problem <br />
+            solvers and support for startups.
+          </p>
+          <div className="buttonsetup">
+            <a href="./register.html">
+              <button>Register Now!</button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <footer className="homefooter">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h6>&copy; 2017 KawoLegal.All Right reserved</h6>
+            </div>
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <div className="logo">
+                <a href="#" className="fa fa-facebook"></a>
+                <a href="#" className="fa fa-twitter"></a>
+                <a href="#" className="fa fa-linkedin"></a>
+                <a href="#" className="fa fa-instagram"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
